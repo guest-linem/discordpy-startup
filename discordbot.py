ffbb6a398d110f7e4172ea0ca6d4474b783e8b69
guest-linem <69501100+guest-linem@users.noreply.github.com>
2020-08-12 09:48:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 import os
 import traceback
+import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+bosstime_list = list()
+dt_now = datetime.datetime.now()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,5 +19,8 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong-now')
 
+@bot.command()
+async def now(ctx):
+    await ctx.send(dt_now)
 
 bot.run(token)
