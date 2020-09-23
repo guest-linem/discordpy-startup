142cf7f57974d871361962a9597b81948be73f65
guest-linem <69501100+guest-linem@users.noreply.github.com>
2020-09-23 14:19:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,11 +3,12 @@ import os
 import traceback
 import datetime
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='\\')
 token = os.environ['DISCORD_BOT_TOKEN']
 bosstime_list = list()
 jst_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 dt_now = jst_now.strftime('%Y年%m月%d日 %H:%M:%S')
+dict_ag = {memo:'',n_time:'',l_time:'',period:'5h',place:'70.黄昏の山脈'}
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -18,7 +19,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong-now')
+    await ctx.send('pong')
 
 @bot.command()
 async def now(ctx):
